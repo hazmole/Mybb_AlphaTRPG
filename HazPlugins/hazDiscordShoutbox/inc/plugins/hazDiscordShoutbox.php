eb67dbbb988a4ffab5836b9fbb260f7dd8f90311
zhutong <buzzfirefox@gmail.com>
2017-06-28 01:30:23 +0800

    shoutbox & nickname

---
+++ HazPlugins/hazDiscordShoutbox/inc/plugins/hazDiscordShoutbox.php
@@ -0,0 +1,276 @@
+<?php
+/**
+ * Haz Phide
+ * Copyright 2015 Hazmole, All Rights Reserved
+ * License: http://www.mybb.com/about/license
+ *
+ * Get a Great help of Sephiroth's [Spoiler MyCode] plugin
+ */
+
+// Disallow direct access to this file for security reasons
+if(!defined("IN_MYBB"))
+{
+    die("Direct initialization of this file is not allowed.");
+}
+
+define('HZDSB_PLUGIN_VER', '0.47');
+/**
+ * Installation Guild:
+ * 		- put "hazHider.php" into "./inc/plugins/"
+ *		- Active "hazHider" in the admin panel.
+ */
+
+function hazDiscordShoutbox_info()
+{
+    return array(
+        "name"          => "Haz Shoutbox with Discord",
+        "description"   => "Integrate Shoutbox with my Discord channel",
+        "website"       => "",
+        "author"        => "Hazmole",
+        "authorsite"    => "",
+        "version"       => HZDSB_PLUGIN_VER,
+        "guid"          => "",
+        "codename"      => str_replace('.php', '', basename(__FILE__)),
+        "compatibility" => "*"
+    );
+}
+
+//$plugins->add_hook("parse_message", "hazhider_run");
+
+
+// Install plugin
+function hazDiscordShoutbox_install() {
+	global $db;
+	// Get SettingGroup DisplayOrder
+	$query	= $db->simple_select("settinggroups", "COUNT(*) as rows");
+	$dis_order = $db->fetch_field($query, 'rows') + 1;
+	// Insert SettingGroup and get Group ID
+	$group_id = $db->insert_query('settinggroups', array(
+		'name'			=> 'hazDiscordShoutbox',
+		'title'			=> 'Haz Shoutbox with Discord',
+		'description'	=> 'display details ...',
+		'disporder'		=> $dis_order,
+		'isdefault'		=> '0'
+	));
+	// Insert Setting
+	$hazDiscordShoutbox_setting = hazDiscordShoutbox_getPluginSettingArray($group_id);
+	$db->insert_query_multiple("settings", $hazDiscordShoutbox_setting);
+	// Rebuild
+	rebuild_settings();
+}
+function hazDiscordShoutbox_uninstall() {
+	global $db;
+	// Get This SettingGroup ID
+	$group_id = $db->fetch_field(
+		$db->simple_select('settinggroups', 'gid', "name='hazDiscordShoutbox'")
+		, 'gid' );
+	// Delete Setting
+	$db->delete_query('settings', 'gid=' . $group_id);
+	$db->delete_query("settinggroups", "name = 'hazDiscordShoutbox'");
+	// Rebuild
+	rebuild_settings();
+}
+function hazDiscordShoutbox_is_installed() {
+	global $db;
+	$query = $db->simple_select("settinggroups", "COUNT(*) as rows", "name = 'hazDiscordShoutbox'");
+	$rows  = $db->fetch_field($query, 'rows');
+	return ($rows > 0);
+}
+
+function hazDiscordShoutbox_activate() {
+	global $db;
+	// Add Global Templates
+	$my_template = hazDiscordShoutbox_getMyTemplateContent();
+	foreach ($my_template as $title => $template) {
+		$new_global_template = array(
+			'title' => $db->escape_string($title), 
+			'template' => $db->escape_string($template), 
+			'sid' => '-1', 
+			'version' => HZDSB_PLUGIN_VER, 
+			'dateline' => TIME_NOW);
+		$db->insert_query("templates", $new_global_template);
+	}
+
+	// Modify Default Templates
+	require_once MYBB_ROOT."/inc/adminfunctions_templates.php";
+	find_replace_templatesets("index",  '#{\$forums}#', "<!-- hazShoutbox -->{\$haz_shoutbox}{\$hazdsb_include}<!-- /hazShoutbox -->{\$forums}");
+}
+function hazDiscordShoutbox_deactivate() {
+	global $db;
+	// Remove Global Templates
+	$my_template = hazDiscordShoutbox_getMyTemplateContent();
+	foreach ($my_template as $title => $template) {
+		$db->delete_query("templates", "`title` = '".$db->escape_string($title)."'");
+	}
+	// Recover Default Templates
+	require_once MYBB_ROOT."/inc/adminfunctions_templates.php";
+	find_replace_templatesets("index", 	'#\<!--\shazShoutbox\s--\>(.+)\<!--\s\/hazShoutbox\s--\>#is', '', 0);
+}
+
+
+
+global $settings;
+
+if ($settings['hazDiscordShoutbox_online']) {
+	$plugins->add_hook('index_start', 'hazDiscordShoutbox_buildShoubox');
+}
+
+function hazDiscordShoutbox_buildShoubox($message) {
+	// Set pattern & replacing string
+	global $lang, $mybb, $templates, $theme, $collapsed, $settings;
+	global $haz_shoutbox, $hazdsb_include, $hazdsb_reply, $hazdsb_invite;
+/*
+	if (!$lang->rinshoutbox) {
+		$lang->load('rinshoutbox');
+	}
+*/
+	if( $mybb->user["uid"]!=0 || $settings['hazDiscordShoutbox_guest'] ){
+		$hazdsb_reply = hazDiscordShoutbox_getReplyTemplate($mybb->user["username"]);
+		$hazdsb_invite= hazDiscordShoutbox_getInviteTemplate( true );
+	}
+	else
+		$hazdsb_invite= hazDiscordShoutbox_getInviteTemplate( false );
+
+	eval("\$haz_shoutbox = \"".$templates->get("hazdsb_shoutbox")."\";");
+	eval("\$hazdsb_include = \"".$templates->get("hazdsb_header_include")."\";");	
+}
+
+
+
+
+function hazDiscordShoutbox_getPluginSettingArray($group_id){
+	$dorder = 1;
+
+	$setting[] = array(
+		'name' => 'hazDiscordShoutbox_online',
+		'title' => 'Shoutbox Online',
+		'description' => 'set shoutbox online or not',
+		'optionscode' => 'yesno',
+		'value' => 1,
+		'disporder' => $dorder++,
+		'gid'		=> $group_id
+	);
+	$setting[] = array(
+		'name' => 'hazDiscordShoutbox_server',
+		'title' => 'Websocket URL',
+		'description' => 'Shoutbox websocket URL',
+		'optionscode' => 'text',
+		'value' => 'wss://hazmoleaws.ddns.net:8001',
+		'disporder' => $dorder++,
+		'gid'		=> $group_id
+	);
+	$setting[] = array(
+		'name' => 'hazDiscordShoutbox_guest',
+		'title' => 'Guest using permission',
+		'description' => 'Can guest using the shoutbox?',
+		'optionscode' => 'yesno',
+		'value' => 1,
+		'disporder' => $dorder++,
+		'gid'		=> $group_id
+	);
+	$setting[] = array(
+		'name' => 'hazDiscordShoutbox_title',
+		'title' => 'Title',
+		'description' => '',
+		'optionscode' => 'text',
+		'value' => '聊天室',
+		'disporder' => $dorder++,
+		'gid'		=> $group_id
+	);
+	$setting[] = array(
+		'name' => 'hazDiscordShoutbox_height',
+		'title' => 'Shoutbox height',
+		'description' => 'height (px)',
+		'optionscode' => 'numeric',
+		'value' => '400',
+		'disporder' => $dorder++,
+		'gid'		=> $group_id
+	);
+
+
+	return $setting;
+}
+
+function hazDiscordShoutbox_getMyTemplateContent(){
+	$my_template['hazdsb_header_include'] = 
+"<script src='{\$mybb->asset_url}/jscripts/hazmole/discordshoutbox/hazDiscordShoutbox.js?ver=".HZDSB_PLUGIN_VER."'></script>
+<script src='http://twemoji.maxcdn.com/twemoji.min.js'></script>  
+<link rel='stylesheet' href='{\$mybb->asset_url}/jscripts/hazmole/discordshoutbox/hazDiscordShoutbox.css?ver=".HZDSB_PLUGIN_VER."' type='text/css'/>
+<script type=\"text/javascript\">
+	hazdsb_server = '{\$mybb->settings['hazDiscordShoutbox_server']}'
+	/*
+	shoutvol = '{\$mybb->settings['rinshoutbox_def_vol']}',
+	iclid = '{\$mybb->settings['rinshoutbox_imgurapi']}',
+	maxnamelength = '{\$mybb->settings['maxnamelength']}',
+	*/
+	\$(document).ready(function() {
+		hazDiscordShoutbox_connect();
+		twemoji.size = '16x16';
+		hazDiscordShoutbox_buildOnline();
+	});
+</script>";
+
+	$my_template['hazdsb_shoutbox'] = 
+"<table border=\"0\" cellspacing=\"0\" cellpadding=\"4\" class=\"tborder tShout\">
+	<thead>
+		<tr>
+			<td colspan=2 class=\"thead\" colspan=\"1\">
+				<div class=\"expcolimage\"><img src=\"{\$theme['imgdir']}/collapse{\$collapsedimg['rshout']}.png\" id=\"rshout_img\" class=\"expander\" alt=\"[-]\" title=\"[-]\" /></div>
+				<div><strong>{\$mybb->settings['hazDiscordShoutbox_title']}</strong></div>
+			</td>
+		</tr>
+	</thead>
+	<tbody id=\"rshout_e\">
+		<tr><td colspan=2 class=\"tcat\"><span class=\"smalltext\"><strong><span>{\$lang->hazDiscordShoutbox_notice_msg}(告示) : </span><span class='notshow'></span></strong></span></td>
+		</tr>
+		<tr>
+			<td class=\"trow2\">
+				<div class=\"contentShout\">
+					<div id=\"hazdsb_shoutarea\" class=\"shoutarea wrapShout\" style=\"height:{\$mybb->settings['hazDiscordShoutbox_height']}px;\"></div>
+				</div>
+			</td>
+			<td style=\"width:200px;\">{\$hazdsb_invite}</td>
+		</tr>
+	</tbody>
+	{\$hazdsb_reply}
+</table>";
+
+	return $my_template;
+}
+
+function hazDiscordShoutbox_getReplyTemplate($user_name){
+	$template = 
+"<tbody>
+	<script>hazdsb_user = \"".preg_quote($user_name)."\"</script>
+	<tr>
+		<td colspan=2><input id=\"hazdsb_shout\" type=\"text\" placeholder=\"說些什麼吧\"></td>
+	</tr>
+</tbody>";
+	return $template;
+}
+
+function hazDiscordShoutbox_getInviteTemplate( $is_member ){
+	global $mybb;
+	$height = $mybb->settings['hazDiscordShoutbox_height'];
+
+	$btn_id    = ($is_member)? "hazdsb_invite_btn": "";
+	$btn_color = ($is_member)? "#555": "red";
+	$btn_text  = ($is_member)? "加入官方聊天室": "註冊加入聊天！";
+
+	$template = 
+"<div style=\"width:100%;height:".$height."px;background:black;color:white;text-align:center;\">
+	<div style=\"padding: 20px 20px 5px 20px;height:40px;background-color:#738bd7;\">
+		<div style=\"width:160px; height:30px;background:url(https://discordapp.com/assets/35d75407bd75d70e84e945c9f879bab8.svg) 50% no-repeat;\"></div>
+	</div>
+	<div style=\"height:20px;font-size:16px; padding:5px;\">在線名單</div>
+	<div id=\"hazdsb_online\" style=\"margin:5px;height:".($height-170)."px;border:1px solid #ccc;overflow-y:auto;text-align:left;\">
+	</div>
+	<a id=\"".$btn_id."\" href=\"./member.php?action=register\" target=\"_blank\">
+		<div style=\"height:20px;width:150px;overflow:hidden;background:".$btn_color.";border-radius:20px;padding:15px;margin:10px;\">
+			<span style=\"font-size:16px;color:white;\">".$btn_text."</span>
+		</div>
+	</a>
+</div>";
+	return $template;
+}
+?>
---
+++ HazPlugins/hazDiscordShoutbox/jscripts/hazmole/discordshoutbox/hazDiscordShoutbox.js
@@ -0,0 +1,101 @@
+
+var hazdsb_ws;
+function hazDiscordShoutbox_connect(){
+	// Check Pre-Setting
+	if( !isWebSoketWork() ){	console.error("Browser Does not support websocket."); return ;}
+	if( hazdsb_server==null ){	console.error("Server url is not set by Admin."); return ;}
+
+
+	// Create websocket
+	hazdsb_ws = new WebSocket(hazdsb_server);
+	hazdsb_ws.onmessage = function (evt) { 
+       var received_msg = evt.data;
+       hazDiscordShoutbox_parseMessages(received_msg);
+    };
+
+    if(document.getElementById("hazdsb_shout"))
+    document.getElementById("hazdsb_shout").addEventListener("keyup", function(event) {
+		event.preventDefault();
+		if (event.keyCode == 13) hazDiscordShoutbox_say();
+	});
+
+}
+
+function hazDiscordShoutbox_parseMessages(data){
+	var json_data = JSON.parse(data);
+	var output_text = "";
+	if(Array.isArray(json_data)){
+		for(var i=0;i<json_data.length;i++){
+			output_text += hazDiscordShoutbox_parseMessage(json_data[i]);
+		}
+	}
+	else{
+		output_text += hazDiscordShoutbox_parseMessage(json_data);
+	}
+	hazDiscordShoutbox_output(output_text);
+	twemoji.parse(document.getElementById("hazdsb_shoutarea"));
+}
+function hazDiscordShoutbox_parseMessage(message){
+	var str = "";
+	var avatar = (message.avatar==null)? "https://discordapp.com/assets/6debd47ed13483642cf09e832ed0bc1b.png": message.avatar;
+	var content = message.content;
+
+	if(message.attachment!=null)
+       for(var i=0;i<message.attachment.length;i++){
+          var url  = message.attachment[i];
+          var link = (isImage(url))? ("<img src='"+url+"' />"): ("附件");
+          content += "<br/><a href='"+url+"' target='_blank'>" + link + "</a>";
+       }
+
+	str += "<span><a href='javascript:hazDiscordShoutbox_mention(\""+message.user_id+"\")'><img class='avatar' src='"+avatar+"'/></a></span>";
+	str += "<b class='user'>" + message.username+" </b>：<span> "+content+" </span>";
+	return "<div class='haz_message'>" + str + "</div>";
+}
+function hazDiscordShoutbox_output(text){
+	var objDiv = document.getElementById("hazdsb_shoutarea");
+	var need_update = false;
+	//Jump
+	if(objDiv.scrollHeight - objDiv.scrollTop - objDiv.clientHeight < 200) need_update = true;
+	objDiv.innerHTML += text;
+	if(need_update) objDiv.scrollTop = objDiv.scrollHeight;
+}
+
+
+function hazDiscordShoutbox_mention(id){
+	var content_box = document.getElementById("hazdsb_shout");
+	if(content_box!=null)	content_box.value += "<@"+id+"> ";
+}
+function hazDiscordShoutbox_say(){
+	var content = document.getElementById("hazdsb_shout").value;
+	if(content=="") return;
+
+	if(hazdsb_user=="" || hazdsb_user==null) hazdsb_user="遊客";
+	var msg = {"type":"custom_message","username":hazdsb_user, "content":content};
+
+	hazdsb_ws.send(JSON.stringify(msg));
+	document.getElementById("hazdsb_shout").value = "";
+}
+
+function hazDiscordShoutbox_buildOnline(){
+	$.ajax({ url: "https://discordapp.com/api/guilds/326606306934915074/widget.json"}).done(function(data) {
+		if(document.getElementById("hazdsb_invite_btn"))
+			document.getElementById("hazdsb_invite_btn").href = data.instant_invite;
+
+		var member_list = "";
+		for(var i=0;i<data.members.length;i++){
+			member_list += hazDiscordShoutbox_buildMember(data.members[i]);
+		}
+		document.getElementById("hazdsb_online").innerHTML = member_list;
+	});
+}
+function hazDiscordShoutbox_buildMember(member){
+	var str = "";
+	str += "<span style=\"padding-right:5px;\"><img src=\""+member.avatar_url+"\" style=\"height:20px;width:20px;border-radius:5px;\"></span>";
+	str += member.username;
+
+	return "<div class=\"hazdsb_member\">"+str+"</div>";
+}
+
+
+function isWebSoketWork(){	return ("WebSocket" in window); }
+function isImage(url){		return (null!=url.match(/\.((png)|(gif)|(jpg)|(bmp))$/));}
