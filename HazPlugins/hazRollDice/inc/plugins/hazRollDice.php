2c09301d1d6988c0bf56f35f5bb9973b4bef4cb7
zhutong <buzzfirefox@gmail.com>
2017-06-27 15:38:47 +0800

    rolldice update

--- HazPlugins/hazRollDice/inc/plugins/hazRollDice.php
+++ HazPlugins/hazRollDice/inc/plugins/hazRollDice.php
@@ -12,7 +12,7 @@ if(!defined("IN_MYBB"))
     die("Direct initialization of this file is not allowed.");
 }
 
-define('HZRD_PLUGIN_VER', '2.5');
+define('HZRD_PLUGIN_VER', '2.658');
 /**
  * Installation Guild:
  * 		- put "hazRollDice.php" into "./inc/plugins/"
@@ -54,7 +54,7 @@ $plugins->add_hook("class_moderation_delete_post","hazRollDice_deleteDiceResult"
 // Install plugin
 function hazRollDice_install() {
 	global $db;
-	$db->write_query("CREATE TABLE mybb_haz_rolldice( pid int(10), uid int(10), type int(10), claim VARCHAR(100), result VARCHAR(200), reason VARCHAR(100))");
+	$db->write_query("CREATE TABLE mybb_haz_rolldice( pid int(10), uid int(10), rid int(10), claim VARCHAR(30), result VARCHAR(300), reason VARCHAR(50))");
 }
 // Uninstall plugin
 function hazRollDice_uninstall() {
@@ -66,17 +66,14 @@ function hazRollDice_is_installed() {
 	if( $db->table_exists("haz_rolldice") )	return true;
 	else	return false;
 }
-
 // activate plugin
 function hazRollDice_activate() {
 	global $db;
-
-	// Add Global Templates 
+	// Add Global Templates
 	$my_template = hazRollDice_getMyTemplateContent();
 	foreach ($my_template as $row) {
 		$db->insert_query("templates", $row);
 	}
-	
 	// Modify Default Templates
 	require_once MYBB_ROOT."/inc/adminfunctions_templates.php";
 	find_replace_templatesets('newreply', '#{\$posticons}#', '{\$posticons}<!-- hazRollDice -->{$hazDice_bar}<!-- /hazRollDice -->');
@@ -89,12 +86,12 @@ function hazRollDice_activate() {
 // deactivate plugin
 function hazRollDice_deactivate() {
 	global $db;
-
+	// Remove Global Templates
 	$my_template = hazRollDice_getMyTemplateContent();
 	foreach ($my_template as $row) {
-		$db->delete_query("templates", "`title` = ".$row["title"]);
+		$db->delete_query("templates", "`title` = '".$row["title"]."'");
 	}
-	
+	// Recover Default Templates
 	require_once MYBB_ROOT."/inc/adminfunctions_templates.php";
 	find_replace_templatesets('newreply', '#\<!--\shazRollDice\s--\>(.+)\<!--\s/hazRollDice\s--\>#is', '', 0);
 	find_replace_templatesets('newthread','#\<!--\shazRollDice\s--\>(.+)\<!--\s/hazRollDice\s--\>#is', '', 0);
@@ -103,40 +100,32 @@ function hazRollDice_deactivate() {
 	find_replace_templatesets('postbit_classic', '#\<!--\shazRollDice\s--\>(.+)\<!--\s/hazRollDice\s--\>#is', '', 0);
 }
 
-
 // For edit/new post, show rolling button.
 function hazRollDice_buildRollBar() {
 	global $lang, $db, $mybb , $templates, $theme, $post;
 	global $hazDice_result, $hazDice_bar;
-	
+	// Load Language pack
 	$lang->load('hazRollDice');
-	
-	$text = "";
-	
+	// Set post_id & user_id
 	$pid = ($post['pid'])? 	$post['pid']: -1;
 	$uid = $mybb->user['uid'];
-	
+	// Fetch Data from DataBase
 	$post_dices_query = $db->simple_select("haz_rolldice", "*", "pid=".$pid);
 	$user_dices_query = $db->simple_select("haz_rolldice", "*", "uid=".$uid." AND pid=0");
+	// Loop building
+	$text = "";
 	$result = null;
 	while(null!=($result = $db->fetch_array($post_dices_query))){
-		$text .= "<tr><td><b>".$result['claim']."</b> => </td>"
-				."<td>".$result['result']." = </td>"
-				."<td><b style=color:blue;>".$result['sum']."</b></td>"
-				."<td>".preg_replace("/\"/", "\\\"", $result['reason'])."</td></tr>";
+		$text .= hazRollDice_getDiceResultTemplate( $result['claim'], $result['result'], $result['reason'], true);
 	}
 	while(null!=($result = $db->fetch_array($user_dices_query))){
-		$text .= "<tr><td>: <b>".$result['claim']."</b> => </td>"
-				."<td>".$result['result']." = </td>"
-				."<td><b style=color:blue;>".$result['sum']."</b></td>"
-				."<td>".preg_replace("/\"/", "\\\"", $result['reason'])."</td></tr>";
+		$text .= hazRollDice_getDiceResultTemplate( $result['claim'], $result['result'], $result['reason'], false);
 	}
 	if($text=="")	$text="<tr><td>--</td></tr>";
-	
+	// Evaluate String to variables
 	eval("\$hazDice_result .= \"".$text."\";");	
-	eval("\$hazDice_bar .= \"".$templates->get("hzrd_bar")."\";");
+	eval("\$hazDice_bar .= \"".$templates->get("hzrd_rollbar")."\";");
 }
-
 
 // For showThread, show each post's Rolling result.
 function hazRollDice_buildDiceResult(&$post) {
@@ -151,24 +140,19 @@ function hazRollDice_buildDiceResult(&$post) {
 		
 		// Set format
 		while(null!=($hazPost = $db->fetch_array($query))){
-			$dice_result = "";
-
 			if($hazPost["claim"][0]=='s'){
-				$dice_result = "<td colspan='3' style='color:white;background:black;text-align:center;'><strong>--[".$lang->hazRollDice_secret_dice."]--</strong></td>";
+				$text .= "<td colspan='3' style='color:white;background:black;text-align:center;'><strong>--[".$lang->hazRollDice_secret_dice."]--</strong></td>"
+				."<td>".preg_replace("/\"/", "\\\"", $hazPost["reason"])."</td></tr>";
 			}
 			else{
-				$dice_result = "<tr><td><strong>".$hazPost["claim"]." =></strong></td>"
-								."<td>".$hazPost["result"]." = </td>"
-							 	."<td><strong style='color:blue;'>".$hazPost["sum"]."</strong></td>";
+				$text .= hazRollDice_getDiceResultTemplate( $hazPost["claim"], $hazPost["result"], $hazPost["reason"], true);
 			}
-			$text .= $dice_result
-					."<td>".preg_replace("/\"/", "\\\"", $hazPost["reason"])."</td></tr>";
 			$count+=1;
 		}
 		
 		if($count){
 			eval("\$post[hazDice_result_text] .= \"".$text."\";");
-			eval("\$post[hazDice_result] .= \"".$templates->get("hzrd_result")."\";");
+			eval("\$post[hazDice_result] .= \"".$templates->get("hzrd_resultbar")."\";");
 		}
 	}
 }
@@ -183,7 +167,6 @@ function hazRollDice_handleResultRecord() {
 			"uid" 		=> $mybb->user['uid'],
 			"claim" 	=> $mybb->get_input("haz_dices_claim"),
 			"result" 	=> $mybb->get_input("haz_dices_result"),
-			"sum" 		=> $mybb->get_input("haz_dices_sum"),
 			"reason" 	=> $db->escape_string($mybb->get_input("haz_dices_reason"))
 			);
 		$db->insert_query('haz_rolldice', $new_record);
@@ -205,6 +188,20 @@ function hazRollDice_setResultRecord() {
 function hazRollDice_deleteDiceResult(){
 	global $db, $pids;
 	$db->delete_query("haz_rolldice", "pid IN ($pids)");
+}
+
+
+function hazRollDice_getDiceResultTemplate($claim, $result, $reason, $is_confirm){
+	if(preg_match( "/→ (.*)$/", $result)==0)
+		$result .= "</td><td>";
+	else
+		$result = preg_replace("/→ (.*)$/", "</td><td> → <b style='color:blue;'>$1</b>", $result);
+
+	$template .= "<tr><td>".($is_confirm? "":": ")
+				."<b>".$claim."</b></td>"
+				."<td> → ".$result."</td>"
+				."<td>".preg_replace("/\"/", "\\\"", $reason)."</td></tr>";
+	return $template;
 }
 
 function hazRollDice_getMyTemplateContent(){
--- HazPlugins/hazRollDice/jscripts/hazmole/rolldices/hz_rolldice.js
+++ HazPlugins/hazRollDice/jscripts/hazmole/rolldices/hz_rolldice.js
@@ -1,58 +1,223 @@
 function haz_rollingDices() {
-	var text   = $("#haz_dices_dice").val();
-	var reason = $("#haz_dices_reason").val();
-	var match= text.match(/^([s]?)(\d+)[dD](\d+)(([+-](\d+))*)/);
-	if(!match){
-    alert("Wrong Format!");
- 	  return;
-  }
-	//console.log("match: "+match);
-	
-	var special_char=match[1];
-	var dice_num =   match[2];
-	var dice_size=   match[3];
-	var dice_offset= match[4];
-	var result = 0;
-	var results = [];
-	var offset_array = [];
-	if(dice_num<0 || dice_size<1) return;
-	if(dice_num>40)		dice_num =40;
-	if(dice_size>100)	dice_size=100;
-	
-	for(var i=0;i<dice_num;i++){
-		results.push(Math.floor((Math.random()*dice_size))+1);
+	var roll_exp    = $("#haz_dices_dice").val();
+	var roll_reason = $("#haz_dices_reason").val();
+
+	if(roll_reason.length>=50){
+		alert("Your reason is too long!");
+		return -1;
 	}
-	
-	
-	$.each(results, function(isx, value){
-		result += parseInt(value);
-	})
-	if((offset_array=dice_offset.match(/[+-]\d+/g))){
-		$.each(offset_array, function(isx, value){
-			result += parseInt(value);
-		})
+
+	// Ignore Fullshape, space, ()
+	roll_exp = roll_exp.replace(/[()（） 　]/g, "");
+	roll_exp = roll_exp.replace(/＜/g, "<").replace(/＞/g, ">").replace(/＝/g, "=").replace(/＋/g, "+").replace(/－/g, "-");
+	roll_exp = roll_exp.replace(/[DｄＤ]/g, "d").replace(/[BｂＢ]/g, "b").replace(/[SｓＳ]/g, "s");
+	if(roll_exp.length>=20){
+		alert("Expression is too long!");
+		return -1;
 	}
+
+	//===========================
+	// Check the rolling format
+	var result_object;
+	result_object = hazrd_analyzeRollFormat(roll_exp);
+	if(result_object<0){
+		alert("Wrong Format!");
+		return -1;
+	}
+	var dice_sets = result_object["dice_sets"]
 	
+	//===========================
+	// Rolling dices
+	var dice_sets_result = [];
+	for(var i=0;i<dice_sets.length;i++){
+		if(dice_sets[i]["type"]==1){
+			var result = [];
+			for(var j=0;j<dice_sets[i]["number"];j++){
+				result.push(Math.floor((Math.random()*dice_sets[i]["dice"]))+1);
+			}
+			dice_sets_result.push(result);
+		}
+		else
+			dice_sets_result.push("");
+	}
+
+	//==========================
+	// Analyze Result
+	var total_sum = 0, number_sum = 0;
+	// :: Sum up numbers
+	for(var i=0; i<dice_sets.length; i++)
+		if(dice_sets[i]["type"]==0) // only effect number
+			number_sum += ((dice_sets[i]["sign"]=="-")? -1: 1) * dice_sets[i]["number"];
+
+	// :: Counting each dices value/ success number
+	for(var i=0; i<dice_sets.length; i++){
+		var set = dice_sets[i];
+		var set_multi = (set["sign"]=="-")? -1: 1;
+
+		var set_sum = 0;
+		if(set["type"]==1){ // only effect dice_result
+			for(var j=0;j<set["number"];j++){
+				if(result_object["is_separate"]){
+					if(result_object["is_compared"]
+					 && eval((dice_sets_result[i][j]+number_sum)+result_object["compare_symbo"]+result_object["compare_val"]))
+					 	set_sum += 1;
+				}
+				else	set_sum += dice_sets_result[i][j];
+			}
+			set["result"] = set_sum;
+			total_sum += set_multi * set["result"];
+		}
+	}
+	if(!result_object["is_separate"])	total_sum += number_sum;
+
+	//==========================
+	// Output
+	var result_string = hazrd_generateRollResult( result_object, dice_sets_result, total_sum, number_sum);
 	
-	haz_rd_updateByDb(match[0], results, dice_offset, result, reason);
+	haz_rd_updateByDb(roll_exp, result_string, roll_reason);
 }
 
-function haz_rd_updateByDb(claim, results, dice_offset, sum, reason) {
+
+function hazrd_generateRollResult(roll_config, dice_sets_result, total_sum, number_sum){
+	var result_string = "";
+
+	var dice_sets   = roll_config["dice_sets"]
+	var is_separate = roll_config["is_separate"];
+	var is_compared = roll_config["is_compared"];
+
+	// each dice set
+	for(var i=0;i<dice_sets.length;i++){
+		var set = dice_sets[i];
+
+		// sign, does not show at the first object
+		if(i!=0 && !(is_separate && set["type"]==0))
+			result_string += " "+set["sign"]+" ";
+
+		if(set["type"]==1){ // dice set
+			if(!is_separate || is_compared)
+				result_string += set["result"];
+			result_string += "[";
+			for(var j=0;j<set["number"];j++){
+				if(j!=0)	result_string += ", ";
+				result_string += dice_sets_result[i][j];
+			}
+			result_string += "]";
+		}
+		else{ //number
+			if(is_separate) continue; // separate mode does not show number offset
+			result_string += set["number"];
+		}
+	}
+
+	if(!is_separate)	result_string += " → " + total_sum;
+	else				result_string = "{"+result_string+"}";
+
+	if(is_separate && number_sum!=0)
+		result_string += ((number_sum>0)?"+":"") + number_sum;
+
+	if(is_compared){
+		result_string += " → ";
+		if(!is_separate)	result_string += (eval(total_sum+roll_config["compare_symbo"]+roll_config["compare_val"]))? "成功": "失敗";
+		else				result_string += total_sum+"成功骰";
+	}
+
+	return result_string;
+}
+function hazrd_analyzeRollFormat(roll_exp){
+	var regex_structure, match_result, content;
+	var return_object = {};
+
+	// Initial
+	return_object["dice_sets"] = [];
+
+	// Check secret/ compare
+	regex_structure = /^([s]?)([bd\d+-]*)(([><=]|(>=)|(<=))(-?\d+))?$/;
+	match_result = roll_exp.match(regex_structure);
+	if(match_result==null)	return -1;
+	// :: analyze
+	return_object["is_secret"]   = (match_result[1]!="");
+	return_object["is_compared"] = (match_result[4]!=null );
+	if(match_result[4]!=null){
+		return_object["compare_symbo"] = (match_result[4]=="=")? "==": match_result[4];
+		return_object["compare_val"]   = parseInt(match_result[7]);
+	}
+	content = match_result[2];
+
+	// Check first dice set format
+	regex_structure = /^(\d+[db]\d+)(.*)$/;
+	match_result = content.match(regex_structure);
+	if(match_result==null)	return -1;
+	// :: analyze
+	var diceset = hazrd_createDiceSet("+", match_result[1]);
+	return_object["dice_sets"].push( diceset );
+	return_object["is_separate"] = (diceset["special"]=="b");
+	content = match_result[2];
+
+	// Check remaind diceset/number format
+	while(content!=""){
+		regex_structure = /^([+-])((\d+[db]\d+)|(\d+))(.*)$/;
+		match_result = content.match(regex_structure);	
+		if(match_result==null)	return -1;
+		// :: analyze
+		var sign =    match_result[1];
+		if(match_result[3]!=null)	return_object["dice_sets"].push( hazrd_createDiceSet(  sign, match_result[3]));
+		else						return_object["dice_sets"].push( hazrd_createNumberSet(sign, parseInt(match_result[4])));
+		content = match_result[5];
+	}
+
+	// :: check consistent
+	var dice_sets = return_object["dice_sets"];
+	for(var i=1;i<dice_sets.length;i++){
+		if(dice_sets[i]["type"]==1 && dice_sets[0]["special"]!=dice_sets[i]["special"])	return -1;
+	}
+
+	return return_object;
+}
+//=============
+// Structure functions
+function hazrd_createDiceSet( sign, dice_expression){
+	var regex = /^(\d+)([db])(\d+)$/;
+	var match_result = dice_expression.match(regex);
+	if(match_result==null)	return null;
+
+	var number = parseInt(match_result[1]);
+	var dice   = parseInt(match_result[3]);
+
+	return {
+		"sign" : sign,
+		"type"  : 1,
+		"number": ((number>20)? 20: number),
+		"dice":   ((dice>100)? 100: dice),
+		"special": match_result[2][0]
+	};
+}
+function hazrd_createNumberSet( sign, number){
+	return {
+		"sign" : sign,
+		"type"  : 0,
+		"number": number
+	};
+}
+
+
+//=============================
+//  Support Functions
+//=============================
+function haz_rd_updateByDb(claim, results, reason) {
 	var data = {
 		action: "hz_rolldice",
 		haz_dices_claim:	claim,
-		haz_dices_result:	"["+results+"]"+dice_offset,
-		haz_dices_sum:		sum,
+		haz_dices_result:	results,
 		haz_dices_reason:	reason
 	};
-	var output = haz_rd_tableRender(claim, results, dice_offset, sum, reason);
+	var output = haz_rd_tableRender(claim, results, reason);
 	
 	var success = function(data){
-		console.log(data);
+		//console.log(data);
 		haz_rd_updateByJquery(output);
 	};
 	var error = function(xhr, status, err) {
-		console.error(status, err.toString());
+		//console.error(status, err.toString());
 	};
 	$.ajax({
 		url: "misc.php",
@@ -74,14 +239,18 @@ function haz_rd_updateByJquery(output) {
 	}
 }
 
-function haz_rd_tableRender(recipe, results, offset, result, reason){
+function haz_rd_tableRender(claim, results, reason){
 	reason = reason.replace(/</g, '&lt;');
 	reason = reason.replace(/>/g, '&gt;');
 	
+	if(results.match(/→(.*)$/)==null)
+		results += "</td><td>";
+	else
+		results = results.replace(/→(.*)$/, '</td><td> → <b style=\'color:blue;\'>$1</b>');
+
 	var TD = '';
-	TD += '<td>: <b>'+recipe+'</b> => </td>';
-	TD += '<td>['+results+']'+offset+' = </td>';
-	TD += '<td><b style=\'color:blue;\'>'+result+'</b></td>';
+	TD += '<td>: <b>'+claim+'</b></td>';
+	TD += '<td> → '+results+'</td>';
 	TD += '<td> '+reason+'</td>';
 	return '<tr>'+TD+'</tr>';
 }
